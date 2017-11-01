7da016b880f28df102ff5a4cd5a37014e5654b89
Mincong <mincong.zhang@hotmail.com>
2017-11-01 20:52:40 +0000

    Update Parser.cxx

--- Parser.cxx
+++ Parser.cxx
@@ -1,24 +1,24 @@
-CfgableParser {
-	private:
-		vector<vector<string> > m_map;
-		unordered_map<string,int> m_field_idx;
+Parser {
+private:
+	vector<vector<string> > m_map;
+	unordered_map<string,int> m_field_idx;
 
-		
-	public:
-		bool load(const string & filename, const config & cfg){
-			//load m_field_idx
-			//load m_map
-		}
 
-		int getFieldIdx(const string & field_name){
-			//if not exist, return -1
-			return m_field_idx[field_name];
-		}
-		
-		//TODO:iterator
-		//TODO:begin()
-		//TODO:end()
-		//TODO:bool iterator::get(const string & field_name, string & field_val)
+public:
+	bool load(const string & filename, const config & cfg){
+		//load m_field_idx
+		//load m_map
+	}
+
+	int getFieldIdx(const string & field_name){
+		//if not exist, return -1
+		return m_field_idx[field_name];
+	}
+
+	//TODO:iterator
+	//TODO:begin()
+	//TODO:end()
+	//TODO:bool iterator::get(const string & field_name, string & field_val)
 };
 
 //Usecase
